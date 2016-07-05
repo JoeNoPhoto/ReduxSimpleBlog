fa0e258b952425a5878bafdcccd32dde702586be
Joe Norris <yourboybstar@gmail.com>
2016-07-05 10:43:52 -0700

    Fetching Data with Lifecycle Methods

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-
 import App from './components/app';
 import PostsIndex from './components/posts_index'
+
 const Greeting = () => {
   return <div>Hey there!</div>;
 };
