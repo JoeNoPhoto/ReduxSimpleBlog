f9bd1ce639fb740390815be27a916c3061933829
Joe Norris <yourboybstar@gmail.com>
2016-07-05 10:52:44 -0700

    Creating New Posts

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    if (!this.props) {
+      return null;
+    }
+    return (
+      <div>Create Form</div>
+    );
+  }
+}
+
+export default PostsNew;
