c62612e52c8afa739261dc6d9774085c3f5c8a32
Joe Norris <yourboybstar@gmail.com>
2016-07-05 18:34:28 -0700

    form validation, createPost action creator, Navigation on Submit

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,6 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
-const Greeting = () => {
-  return <div>Hey there!</div>;
-};
-
 
 export default (
   <Route path="/" component={App} >
