8f20802d7bf426050e707bb5b685190aad168d6d
Joe Norris <yourboybstar@gmail.com>
2016-07-06 01:12:19 -0700

    Delete Action Creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=sjhsaljkfdhsa';
@@ -25,12 +26,20 @@ export function createPost(props) {
   };
 }
 
-export function fetchPost(id){
+export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}}`);
 
   return {
     type: FETCH_POST,
     payload: request,
   };
+}
 
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
+    payload: request,
+  };
 }
